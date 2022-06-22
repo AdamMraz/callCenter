e8ca18eaf685a93c3b87bd61d852d86f0b3a4330
AdamMraz <kadkot555@gmail.com>
2022-06-22 23:32:43 +0700

    refactoring and add README.md

--- src/main/java/ru/adamDev/callCenter/service/service/impl/TaskManagerServiceImpl.java
+++ src/main/java/ru/adamDev/callCenter/service/service/impl/TaskManagerServiceImpl.java
@@ -42,7 +42,6 @@ public class TaskManagerServiceImpl implements TaskManagerService {
      */
     @Override
     public void saveTask(Task task) throws TaskIsExistException, TaskIllegalNumberException {
-
         if (task.getNumber() <= 0) {
             LOG.warn("The task number cannot be a negative number or 0");
             throw new TaskIllegalNumberException("The task number cannot be a negative number or 0");
@@ -51,18 +50,15 @@ public class TaskManagerServiceImpl implements TaskManagerService {
             LOG.warn("A task with this number already exists");
             throw new TaskIsExistException("A task with this number already exists");
         }
-
         taskRepo.save(task);
     }
 
     @Override
     public List<Task> finedTasks(TaskFilter filter) throws TaskFilterDateIsNullException {
-
         if (filter.getStartDate() == null || filter.getFinishDate() == null) {
             LOG.warn("The start and end dates cannot be empty");
             throw new TaskFilterDateIsNullException("The start and end dates cannot be empty");
         }
-
         return taskRepo.findByFilter(filter.getStartDate(),
                 filter.getFinishDate(),
                 filter.getNumber(),
@@ -71,25 +67,20 @@ public class TaskManagerServiceImpl implements TaskManagerService {
 
     @Override
     public void updateTask(UpdateTask updateTaskModel) throws TaskIllegalNumberException, TaskNotFoundException {
-
         if (updateTaskModel.getTaskId() <= 0) {
             LOG.warn("The task number cannot be a negative number or 0");
             throw new TaskIllegalNumberException("The task number cannot be a negative number or 0");
         }
-
         Task task = taskRepo.findById(updateTaskModel.getTaskId()).orElse(null);
-
         if (task == null) {
             LOG.warn("The task with current id not fount");
             throw new TaskNotFoundException("The task with current id not fount");
         }
-
         if (updateTaskModel.getStatus() != null)
             task.setStatus(updateTaskModel.getStatus());
         if (updateTaskModel.getComment() != null)
             task.setComment(updateTaskModel.getComment());
         task.setUpdateDate(new Date());
-
         taskRepo.save(task);
     }
 }
